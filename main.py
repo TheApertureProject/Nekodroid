5c86b465512a90a29a60b1e761371ae2a2ca7680
The Aperture Project <apertureproject.me@gmail.com>
2018-12-15 00:19:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ bot.ready = False
 
 async def status():
     while True:
-        names = [f'{PREFIX}help', 'on AP3RTURE', f'on {len(bot.guilds)} servers', f'with {len(bot.users)}']
+        names = [f'{PREFIX}help', 'on AP3RTURE', f'on {len(bot.guilds)} servers', f'with {len(bot.users)} users']
         for name in names:
             await bot.change_presence(activity=discord.Game(name=name))
             await asyncio.sleep(7)
