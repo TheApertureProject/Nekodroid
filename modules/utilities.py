8f4242b7157e992bc854474e7fef4fc3dcaa778c
The Aperture Project <apertureproject.me@gmail.com>
2018-12-14 22:41:52 +0100

    Update utilities.py
    Added a wikipedia search command

--- modules/utilities.py
+++ modules/utilities.py
@@ -8,12 +8,15 @@ class Utilities:
         self.bot = bot
         self.config = bot.config
 
-    @commands.guild_only()
     @commands.command()
     async def pp(self, ctx, usr: discord.User):
         e = discord.Embed(description=f"[{usr.name}'s profile picture]({usr.avatar_url})", title='Avatar', color=0x5D5DFF)
         e.set_image(url=usr.avatar_url)
         await ctx.send(embed=e)
+
+    @commands.command()
+    async def wiki(self, ctx, *, searchterm):
+        await ctx.send(f"***Wikipedia Search*** 🔀 https://en.wikipedia.org/wiki/{searchterm}")
 
 def setup(bot):
     bot.add_cog(Utilities(bot))
