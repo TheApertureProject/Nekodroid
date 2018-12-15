03987a3f1b7c4fdbe9def960379b3f432b496390
The Aperture Project <apertureproject.me@gmail.com>
2018-12-15 01:29:47 +0100

    Add files via upload

--- modules/general.py
+++ modules/general.py
@@ -2,22 +2,14 @@ import discord
 from discord.ext import commands
 
 class General:
-    
+
     def __init__(self, bot):
         self.bot = bot
         self.config = bot.config
-    
+
     @commands.command()
     async def ping(self, ctx):
         await ctx.send('Pong !')
-    
-    @commands.command()
-    async def pong(self, ctx):
-        await ctx.send('Ping !')
-    
-    @commands.command(aliases=['add'])
-    async def invite(self, ctx):
-        await ctx.send('***Invite Link 🔀 <https://discordapp.com/oauth2/authorize?client_id=467332623677521940&scope=bot&permissions=2146958591>***
 
 def setup(bot):
     bot.add_cog(General(bot))
