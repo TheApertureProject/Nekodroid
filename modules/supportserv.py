a0e80842adb7eafd275e800f0c8ecf0e8f1ac743
The Aperture Project <apertureproject.me@gmail.com>
2018-12-21 10:06:37 +0100

    Update supportserv.py
    Added a kawaii command to get kawaii role on support server

--- modules/supportserv.py
+++ modules/supportserv.py
@@ -2,3 +2,18 @@ import discord
 from discord.ext import commands
 
 class SupportServ:
+    
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = bot.config
+        
+    @commands.command()
+    async def kawaii(self, ctx):
+        if ctx.guild.id==462871882916560896:
+            await ctx.user.add_role('KawaiiRP')
+            await ctx.send('Role added. Just visit <#525421450241376266> to get the RP started !')
+        else:
+            await ctx.send("I'm sorry, but this command can be used on Kanna's support server only. Join here → https://discord.gg/PTT9UpZ")
+
+def setup(bot):
+    bot.add_cog(SupportServ(bot))
