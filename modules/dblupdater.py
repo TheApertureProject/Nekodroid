30bb0392395f5afe607f7d9a947188f11a6558ad
The Aperture Project <apertureproject.me@gmail.com>
2018-12-23 13:53:49 +0100

    Update dblupdater.py
    Corrected an indentation error, that was leading to a spam of the dbl API

--- modules/dblupdater.py
+++ modules/dblupdater.py
@@ -22,7 +22,7 @@ class DiscordBotsOrgAPI:
                 await self.dblpy.post_server_count()
             except Exception as e:
                 print(e.args)
-                await asyncio.sleep(1800)
+            await asyncio.sleep(1800)
 
 def setup(bot):
     global logger
