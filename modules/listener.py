bb16ab6ab662ba10e89f41755aa5a030ef9bed0f
The Aperture Project <apertureproject.me@gmail.com>
2018-12-29 16:08:24 +0100

    Update listener.py

--- modules/listener.py
+++ modules/listener.py
@@ -23,10 +23,10 @@ class Listeners:
         if member.guild.id == 462871882916560896:
             role = discord.utils.get(member.guild.roles, name='Members')
             await member.add_roles(role)
-        e=discord.Embed(description="Thanks for joining my support server !", title=f'Welcome, {member.name} !', color=0xdb90f4)
-        e.set_thumbnail(url='https://cdn.discordapp.com/attachments/476653267036930049/528247247574401025/WindowKamuis.gif')
-        e.set_image(url='https://cdn.discordapp.com/attachments/476653267036930049/528247286598467614/train-girl.jpg')
-        await member.send(embed=e)
+            e=discord.Embed(description="Thanks for joining my support server !", title=f'Welcome, {member.name} !', color=0xdb90f4)
+            e.set_thumbnail(url='https://cdn.discordapp.com/attachments/476653267036930049/528247247574401025/WindowKamuis.gif')
+            e.set_image(url='https://cdn.discordapp.com/attachments/476653267036930049/528247286598467614/train-girl.jpg')
+            await member.send(embed=e)
 
     async def on_guild_join(self, guild):
         join = self.bot.get_channel(462875598184775700)
