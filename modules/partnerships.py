1fe0676ecb4c6179496196f9d744295d8d861da6
The Aperture Project <apertureproject.me@gmail.com>
2019-01-05 02:54:40 +0100

    Update partnerships.py

--- modules/partnerships.py
+++ modules/partnerships.py
@@ -37,7 +37,7 @@ Rejoignez dès maintenant Aperture, le tout nouveau serveur dédié aux anime, a
 🎉 Des **giveaways** !
 
 🤖 Un bot dédié au serveur, accessible à tous :3"""
-        e = discord.Embed(name='Join server', descritption=a, color=0x1a53ff, url='https://discord.gg/JEUUM8c')
+        e = discord.Embed(name='Join server', description=a, color=0x1a53ff, url='https://discord.gg/JEUUM8c')
         e.set_image(url='https://cdn.discordapp.com/attachments/466600971213209602/473418748959653888/20180730_111606.gif')
         e.set_thumbnail(url='https://cdn.discordapp.com/attachments/489041727697584148/503466180481384448/1528224039633.png')
         await chan.send(embed=e)
