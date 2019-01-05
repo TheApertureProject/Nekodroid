12d56460130f78ebc65e73eaf07f34836e1a8a73
The Aperture Project <apertureproject.me@gmail.com>
2019-01-05 19:39:23 +0100

    Update partnerships.py

--- modules/partnerships.py
+++ modules/partnerships.py
@@ -40,14 +40,13 @@ Rejoignez dès maintenant Aperture, le tout nouveau serveur dédié aux anime, a
         e = discord.Embed(name='Join server', description=a, color=0x1a53ff, url='https://discord.gg/JEUUM8c')
         e.set_image(url='https://cdn.discordapp.com/attachments/466600971213209602/473418748959653888/20180730_111606.gif')
         e.set_thumbnail(url='https://cdn.discordapp.com/attachments/489041727697584148/503466180481384448/1528224039633.png')
+        await chan.send(embed=e)
         if mentionning == 'here':
-            await chan.send(embed=e)
             await chan.send('@here')
         elif mentionning == 'everyone':
-            await chan.send(embed=e)
             await chan.send('@everyone')
         else :
-            await chan.send(embed=e)
+            pass
 
 def setup(bot):
     bot.add_cog(Partnership(bot))
