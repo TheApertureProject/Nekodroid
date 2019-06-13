02402f5e1e411a35844167eaa61cd7665b92b4df
The Aperture Project <apertureproject.me@gmail.com>
2019-06-13 18:51:59 +0200

    Update moderator.py

--- modules/moderator.py
+++ modules/moderator.py
@@ -13,28 +13,19 @@ class Moderator(commands.Cog):
     @commands.has_permissions(ban_members=True)
     @commands.command()
     async def ban(self, ctx, member: discord.Member, *, reason: str = None):
-        try:
-            if reason==None:
-                await member.ban()
-                await ctx.send('Member'+member+'was successfully banned ! Good bye !')
-                await ctx.send('And dont come back !')
-            else:
-                await member.ban(reason=reason)
-                await ctx.send(f'Member'+member+'was successfully banned for the following reason : {reason} ! Good bye !')
-        except Exception as e:
-            print(e.args)
-            await ctx.send('An error occured. Check your permissions & the way you wrote the username.')
+        if reason==None:
+            await member.ban()
+            await ctx.send('✅ | Member'+member+'was successfully banned ! Good bye !')
+        else:
+            await member.ban(reason=reason)
+            await ctx.send(f'✅ | Member {member} was successfully banned for the following reason : {reason} ! Good bye !')
 
     @commands.guild_only()    
     @commands.has_permissions(kick_members=True)
     @commands.command()
     async def kick(self, ctx, *, member: discord.Member):
-        try:
-            await member.kick()
-            await ctx.send('Member', member, 'was successfully kicked ! Babaï !')
-        except Exception as e:
-            print(e.args)
-            await ctx.send('An error occured. Check your permissions & the way you wrote the username.')
+        await member.kick()
+        await ctx.send('Member', member, 'was successfully kicked ! Babaï !')
 
     @commands.guild_only()
     @commands.has_permissions(manage_messages=True)
@@ -43,9 +34,9 @@ class Moderator(commands.Cog):
         await ctx.message.delete()
         try:
             deleted = await ctx.channel.purge(limit=amount)
-            await ctx.send(f"`{len(deleted)}` messages successfully deleted !", delete_after = 5)
-        except:
-            await ctx.send("Something went wrong. Please retry indicating positive numbers only, and check your permissions.")
+            await ctx.send(f"✅ | `{len(deleted)}` messages successfully deleted !", delete_after = 5)
+        except HTTPException:
+            await ctx.send("❎ | I'm sorry, nya ! I can not clear messages older than two weeks.")
 
 def setup(bot):
     bot.add_cog(Moderator(bot))
