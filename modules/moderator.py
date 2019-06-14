31c6eac1a9ee88e34eda90e2dd76dedc4e2d5365
The Aperture Project <apertureproject.me@gmail.com>
2019-06-14 22:09:50 +0200

    Update moderator.py

--- modules/moderator.py
+++ modules/moderator.py
@@ -38,7 +38,6 @@ class Moderator(commands.Cog):
         except discord.HTTPException:
             await ctx.send(f"❎ | I'm sorry, but I can't delete messages older than two weeks. Nya !")
 
-
     @commands.guild_only()
     @commands.has_permissions(manage_roles=True)
     @commands.command()
@@ -51,19 +50,18 @@ class Moderator(commands.Cog):
             await guild.create_role(name="Muted")
             await ctx.send('✅ | The `Muted` role was succesfully created !')
 
-
     @commands.guild_only()
     @commands.has_permissions(manage_roles=True)
     @commands.command()
-    async def mute(self, ctx, member=discord.Member, reason):
+    async def mute(self, ctx, usr=discord.Member, REASON):
         muted = discord.utils.get(ctx.guild.roles, name='Muted')
         if muted is not None:
             role = discord.utils.get(name="Muted", ctx.guild.roles)
-            await member.add_roles(role)
-            if reason == None:
-                await ctx.send(f'✅ | Member `{member}` successfully muted, nya !')
+            await usr.add_roles(role)
+            if REASON == None:
+                await ctx.send(f'✅ | Member `{usr}` successfully muted, nya !')
             else:
-                await ctx.send(f'✅ | Member `{member}` successfully muted for the following reason :```{reason}``` Nya !')
+                await ctx.send(f'✅ | Member `{usr}` successfully muted for the following reason :```{REASON}``` Nya !')
         else:
             await ctx.send('❎ | The `Muted` role doesn\'t exist. Please create it first, or let me do that for you by typing `nya!setmute`.')
 
