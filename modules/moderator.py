a765276be994fb4ee534978f47afe1f14617a291
The Aperture Project <apertureproject.me@gmail.com>
2019-06-14 21:47:27 +0200

    Update moderator.py
    [Yet not stable]

--- modules/moderator.py
+++ modules/moderator.py
@@ -36,7 +36,36 @@ class Moderator(commands.Cog):
             deleted = await ctx.channel.purge(limit=amount)
             await ctx.send(f"✅ | `{len(deleted)}` messages successfully deleted !", delete_after = 5)
         except discord.HTTPException:
-            await ctx.send("❎ | I'm sorry, nya ! I can not clear messages older than two weeks.")
+            await ctx.send(f"❎ | I'm sorry, but I can't delete messages older than two weeks. Nya !")
+
+
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles=True)
+    @commands.command()
+    async def mute(self, ctx, member=discord.Member, reason):
+        muted = discord.utils.get(ctx.guild.roles, name='Muted')
+        if muted is not None:
+            await ctx.send('❎ | The `Muted` role was created already, nya.')
+        else:
+            guild = ctx.guild
+            await guild.create_role(name="Muted")
+            await ctx.send('✅ | The `Muted` role was succesfully created !')
+
+
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles=True)
+    @commands.command()
+    async def mute(self, ctx, member=discord.Member, reason):
+        muted = discord.utils.get(ctx.guild.roles, name='Muted')
+        if muted is not None:
+            role = discord.utils.get(name="Muted", ctx.guild.roles)
+            await member.add_roles(role)
+            if reason == None:
+                await ctx.send(f'Member `{member}` successfully muted, nya !')
+            else:
+                await ctx.send(f'Member `{member}` successfully muted for the following reason :```{reason}``` Nya !')
+        else:
+            await ctx.send('The `Muted` role doesn\'t exist. Please create it first, or let me do that for you by typing `nya!setmute`.')
 
 def setup(bot):
     bot.add_cog(Moderator(bot))
