73263c29b9d4748e7bb06ec4769492d3cf09f4f1
The Aperture Project <apertureproject.me@gmail.com>
2019-06-14 22:03:55 +0200

    Update moderator.py

--- modules/moderator.py
+++ modules/moderator.py
@@ -42,7 +42,7 @@ class Moderator(commands.Cog):
     @commands.guild_only()
     @commands.has_permissions(manage_roles=True)
     @commands.command()
-    async def setmute(self, ctx, member=discord.Member, reason):
+    async def setmute(self, ctx):
         muted = discord.utils.get(ctx.guild.roles, name='Muted')
         if muted is not None:
             await ctx.send('❎ | The `Muted` role was created already, nya.')
