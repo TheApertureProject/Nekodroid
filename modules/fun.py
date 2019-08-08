8fa67ca55a304f28b4e163da2938d31e42dd2725
The Aperture Project <apertureproject.me@gmail.com>
2019-08-08 23:31:46 +0200

    Update fun.py

--- modules/fun.py
+++ modules/fun.py
@@ -48,7 +48,7 @@ class Fun(commands.Cog):
         some_fact = nekos.fact()
         e = discord.Embed(title = "Facts", description=some_fact, color=0x0000EE)
         e.set_thumbnail(url="https://cdn.discordapp.com/attachments/560801259968266240/609132350051188749/1565298690301.png")
-        await ctx.send(e=embed)
+        await ctx.send(embed=e)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
