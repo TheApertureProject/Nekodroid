7977677ecccc89a851a345ac76bbe9b9d91c0fa3
The Aperture Project <apertureproject.me@gmail.com>
2019-08-18 17:50:16 +0200

    Update listener.py

--- modules/listener.py
+++ modules/listener.py
@@ -110,13 +110,13 @@ Guild Nr. `{len(self.bot.guilds)}`"""
         elif isinstance(error, commands.DisabledCommand):
             await ctx.send(f'{redcross} | This command is not usable right know due to a bug.')
         else:
-            embedbasic = discord.Embed(color=discord.Color.red(), description='⚠ An unknown error occured! The error will be fixed as soon as possible. Please join our **[support server](https://discord.gg/PTT9UpZ)** if you think you can give us details about the error c:')
+            embedbasic = discord.Embed(color=discord.Color.red(), description='⚠ An unknown error occured! The error will be fixed as soon as possible. Please join our **[support server](https://discord.gg/wxBGu5f)** if you think you can give us details about the error c:')
             errorembed = discord.Embed(color=discord.Color.red(), title=f'Error caused by `{ctx.author}` ({ctx.author.id})', description=f'```py\n{error}\n```')
             errorembed.add_field(name='Server', value=f'**`{ctx.guild.name}`** ({ctx.guild.id})', inline=True)
             errorembed.add_field(name='Command', value=f'**{ctx.command.name}**')
             join = self.bot.get_channel(SJCHAN_ID)
             try:
-                invite_url = await bot.get_channel(bot.get_guild(ID).system_channel).create_invite()
+                invite_url = await self.bot.get_channel(self.bot.get_guild(ID).system_channel).create_invite()
                 invite = f"[Join this server]({invite_url.url})"
             except:
                 invite = "Couldn't generate any invite for this server."
