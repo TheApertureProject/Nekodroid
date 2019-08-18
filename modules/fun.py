59676fe60cd72c71b3806c95ccebfcb03574e48f
The Aperture Project <apertureproject.me@gmail.com>
2019-08-18 21:32:04 +0200

    Update fun.py

--- modules/fun.py
+++ modules/fun.py
@@ -68,7 +68,9 @@ class Fun(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if 'neko' in message.content:
-            await message.channel.send(nekos.textcat())
+            await message.channel.send(f'`{nekos.textcat()}`')
+        if 'nekodroid' in message.content:
+            await message.add_reaction('<:lurk:612729955830333595>')
 
 def setup(bot):
     bot.add_cog(Fun(bot))
