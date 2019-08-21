870e6523a37b3b3ea6918152a308cb0b39497329
The Aperture Project <apertureproject.me@gmail.com>
2019-08-21 17:20:39 +0200

    Added an osu stats command (not done)
    Added an osu stats command (not done)

--- modules/stats.py
+++ modules/stats.py
@@ -1,2 +1,28 @@
 import discord
 from discord.ext import commands
+import json
+
+osu_api_key = os.environ["OSUTOKEN"]
+
+class Stats(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = bot.config
+    
+    @commands.command()
+    async def osu(self, ctx, player_id):
+        r = requests.get(url=f'https://osu.ppy.sh/p/api/get_user?k={osu_api_key}&u={player_id}')
+        data = (await r.json())[0]
+        user_name=data["user_name"]
+        user_id=data["user_id"]
+        join_date=data["join_date"]
+        playcount=data["playcount"]
+        pp_raw=data["pp_raw"]
+        pp_rank=data["pp_rank"]
+        level=["level"]
+        country=["country"]
+        e = discord.Embed(title = f'osu! Profile for user {user_name}', description=f'Player ID : {user_id}', url = f'https://osu.ppy.sh/users/{user_id}', color = 0xFF69B4)
+
+def setup(bot):
+    bot.add_cog(Stats(bot))
