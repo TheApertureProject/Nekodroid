b255ca5e6dfe38b6113d12770cb8daa7b704687a
poulpette <apertureproject.me@gmail.com>
2020-03-16 17:27:42 +0100

    Update datacom.py

--- modules/datacom.py
+++ modules/datacom.py
@@ -22,7 +22,7 @@ class Datacom(commands.Cog):
 
     @commands.command()
     async def register(self, ctx):
-        z = discord.Embed(title = "<a:loading:684031670520643640> | Creating your profile")
+        z = discord.Embed(title = "<a:loading:684031670520643640> Creating your profile")
         y = await ctx.send(embed = z)
         
         playerid = ctx.author.id
@@ -30,31 +30,32 @@ class Datacom(commands.Cog):
         user1 = db.profiles.find_one({'ui' : playerid})
         
         if user1 is not None:
-            a = discord.Embed(title="⚠️ | You've already created an account.")
+            a = discord.Embed(title="⚠️ You've already created an account.")
             await y.edit(embed=a)
+
         else:
             UNAME = ctx.author.name
             UDESC = "I'm a pretty chill person !"
             UMONEY = 1000
             ULEVEL = 1
         
-        profile = {'ui' : playerid,
-        'ud' : UDESC,
-        'um' : UMONEY,
-        'ul' : ULEVEL,
-        }
+            profile = {'ui' : playerid,
+            'ud' : UDESC,
+            'um' : UMONEY,
+            'ul' : ULEVEL,
+            }
 
-        db.profiles.insert_one(profile)
+            db.profiles.insert_one(profile)
+
+            a = f"""`{UDESC}`
+            Balance : {UMONEY} <:nekocoins:689129268135067648>
+            Level : {ULEVEL}"""
         
-        a = f"""**{UNAME}**'s profile
-        `{UDESC}`
-        Balance : {UMONEY} <a:nekocoins:689129268135067648>
-        Level : {ULEVEL}"""
+            e = discord.Embed(title = "✔️ Neko-profile created !", color = 0x16c60c)
+            e.add_field(name = f"**{UNAME}**'s profile", value = a)
+            e.set_footer(text = "Tip : edit your description by typing 'nya desc <Description>' !")
         
-        e = discord.Embed(title = "✔️ | Neko-profile created !", description = a, color = 0x16c60c)
-        e.set_footer(text="Tip : edit your description by typing 'Nya desc <Description>' !")
-        
-        await y.edit(embed = e)
+            await y.edit(embed = e)
         
 def setup(bot):
     bot.add_cog(Datacom(bot))
